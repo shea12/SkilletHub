698b6be47ffb6d8815edc9f9adb965f33f16f68d
Justin Griffin <jbg46209@gmail.com>
2016-09-27 11:36:41 -0700

    Make outline of tests for versionsController

--- spec/server/controllers/versionsControllerSpec.js
+++ spec/server/controllers/versionsControllerSpec.js
@@ -5,30 +5,105 @@ var ctrl = require('../../../server/controllers/versionsController.js');
 var createVersion = ctrl.createVersion;
 var getVersion = ctrl.getVersion;
 var deleteVersion = ctrl.getVersion;
+var req, res;
 
 describe('versionsController.js', function() {
   describe('createVersion()', function() {
+    beforeEach(function() {
+      req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/user/recipe/branch/version',
+        body: {
+
+        }
+      });
+      res = httpMocks.createResponse()
+    });
+
     it('should be a function', function() {
       expect(createVersion).to.be.a('function');
     });
-    //should respond with success if creation successful
-    //should respond with error if creation unsuccessful
+    it('should save to the correct user', function() {
+
+    });
+    it('should save to the correct branch', function() {
+
+    });
+    it('should omit fields that are not modified', function() {
+
+    });
+    it('should have a reference to the root version', function() {
+
+    });
+    it('should have a reference to the previous version', function() {
+
+    });
+    it('should respond with success if creation handled successfully', function() {
+
+    });
+    it('should respond with an error if creation handled unsuccessfully', function() {
+
+    });
+    it('should maintain ingredient order', function() {
+
+    });
+    it('should maintain step order', function() {
+
+    });
   });
 
   describe('getVersion()', function() {
+    beforeEach(function() {
+      req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/user/recipe/branch/version',
+      });
+      res = httpMocks.createResponse()
+    });
+
     it('should be a function', function() {
       expect(getVersion).to.be.a('function');
     });
-    //should respond with error if get unsuccessful
+    it('should return a complete recipe', function() {
+
+    });
+    it('should return the correct version', function() {
+
+    });
+    it('should respond with a 404 if no results', function() {
+      expect().to.be.equal(404);
+    });
+    it('should respond with error if unable to get results', function() {
+
+    });
   });
 
   describe('deleteVersion()', function() {
+    beforeEach(function() {
+      req = httpMocks.createRequest({
+        method: 'DELETE',
+        url: '/user/recipe/branch/version',
+      });
+      res = httpMocks.createResponse()
+    });
+
     it('should be a function', function() {
       expect(deleteVersion).to.be.a('function');
     });
-    //should delete a version if no branches or forks below
-    //should not return a version if branches or forks below
-    //should respond with success if deletion successful
-    //should respond with error if deletion unsuccessful
+    it('should delete the correct version', function() {
+
+    });
+    it('should delete a version only if no branches or forks depend on it', function() {
+
+    });
+    it('should be unavailable to user, even if not actually deleted', function() {
+
+    });
+    it('should respond with success if deletion successful', function() {
+
+    });
+    it('should respond with error if deletion unsuccessful', function() {
+
+    });
   });
 });
