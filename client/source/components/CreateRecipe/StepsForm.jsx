d06ea06bc434965e1a56bf1ed94ce3347a862364
austinmr <austin.riedel@gmail.com>
2016-09-27 12:49:22 -0700

    Implement CreateRecipe components that allow user to enter new recipe

--- client/source/components/CreateRecipe/StepsForm.jsx
+++ client/source/components/CreateRecipe/StepsForm.jsx
@@ -1,42 +1,75 @@
 import React from 'react';
+import AddStep from './AddStep'; 
 
 class StepsForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			changed: true, 
-			description: '',
-			ingredients: []
+			steps: [],
+			stepsCount: 1,
+			newStep: []
 		}; 
 	}
 
-	handleSubmit (event) {
-		event.preventDefault(); 
-		this.props.handleAddStep(this.state);
-		var next = this.state.count; 
-		next++; 
+	componentWillMount() {
+		console.log('Steps Form is mounting!'); 
 		this.setState({
-		});
+			steps: [], 
+			newStep: [{
+				changed: true, 
+				description: 'Placeholder',
+				ingredients: [],
+				position: null
+			}]
+		}); 
+	}
+
+	handleAddStep (step) {
+		event.preventDefault(); 
+		var steps = this.state.steps; 
+		// console.log('Currents steps:', JSON.stringify(steps)); 
+		// console.log('NEW STEP: ', step); 
+		// steps[steps.length - 1] = step; 
+		steps.push(step); 
+		// console.log('Steps after change: ', steps); 
+		// steps.push({
+		// 		changed: true, 
+		// 		description: 'Placeholder',
+		// 		ingredients: []
+		// 	}); 
+		var stepsCount = this.state.stepsCount + 1; 
+		this.setState({
+			steps: steps,
+			stepsCount: stepsCount
+		}); 
+		// console.log('Added a step!'); 
 	}
 
 	handleChange (event) {
-	  var inputType = event.target.className; 
-	  if (inputType === 'exerciseInput') {
-	  	this.setState({exercise: event.target.value}); 
-	  } else if (inputType === 'warmupsInput') {
-	  	this.setState({warmups: event.target.value}); 
-	  } else {
-	  	this.setState({sets: event.target.value}); 
-	  }
+	  // var inputType = event.target.className; 
+	  // if (inputType === 'exerciseInput') {
+	  // 	this.setState({exercise: event.target.value}); 
+	  // } else if (inputType === 'warmupsInput') {
+	  // 	this.setState({warmups: event.target.value}); 
+	  // } else {
+	  // 	this.setState({sets: event.target.value}); 
+	  // }
 	}
 
 	render () {
 		return (
-			<div className="row">
-			  <div className="input-field col s12">
-			    <textarea id="description" className="materialize-textarea"></textarea>
-			    <label htmlFor="description">Step Description</label>
-			  </div>
+			<div>
+				<h3> Recipe Steps </h3>
+				{this.state.steps.map((step) => (
+					<div key={'enteredStep' + step.position}>
+					  <h4> Step {step.position} </h4>
+					  <h5> {step.description} </h5>
+					  <h5> {step.ingredients.join(',')} </h5>
+					</div>
+				))}
+				{this.state.newStep.map((step) => (
+					<AddStep key={'step' + this.state.stepsCount} stepNumber={this.state.stepsCount} handleAddStep={this.handleAddStep.bind(this)} step={step}/>
+				))}
 			</div>
 		); 
 	}
