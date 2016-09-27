02980ed472e22cb3629a0d9a8d7ea3352f97d1bf
Justin Griffin <jbg46209@gmail.com>
2016-09-27 12:39:38 -0700

    Add planned helper functions

---
+++ server/helpers.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //build a version and save it
+  makeVersion: (changes) => {
+
+  },
+  //retrieve a particular version
+  retrieveVersion: (version) => {
+
+  },
+  //from a particular point,
+  //for all version below it with no dependencies, delete them
+  //for everything else, set them to unavailable
+  deleteDownstream: (version) => {
+
+  }
+};
