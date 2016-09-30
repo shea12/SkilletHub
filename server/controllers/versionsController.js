d225e308fcc1414fb52165b51826b23e92b6ec0c
Justin Griffin <jbg46209@gmail.com>
2016-09-30 16:39:11 -0700

    Modify createVersion to update the UserRecipes

--- server/controllers/versionsController.js
+++ server/controllers/versionsController.js
@@ -1,4 +1,6 @@
 let helpers = require(`${__dirname}/../helpers.js`);
+let UserRecipe = require(`${__dirname}/../schemas.js`).UserRecipe;
+let _ = require('underscore');
 
 module.exports = {
   // Description: Makes a new version from an existing version
@@ -9,11 +11,30 @@ module.exports = {
   //   }
   // Output: Created version
   createVersion: (req, res) => {
+    let newVersion;
     helpers.makeVersion(req.body.previous, req.body.changes)
-    .then(result => {
-      res.status(201).send(result);
-    })
-    .catch(error => {
+    .then((version) => {
+      newVersion = version;
+      return //userid where username
+    }).then((userId) => {
+      return UserRecipe.find({
+        userId: userId
+      });
+    }).then(userRecipe => {
+      let recipeLocation = _.findIndex(userRecipe.recipes, 
+        recipe => recipe.rootRecipeId === newVersion.rootRecipeId);
+      let recipe = userRecipe[recipeLocation];
+      if (newVersion.branch = 'master') {
+        recipe.name = newVersion.name;
+      }
+
+      let branchLocation = _.findIndex(recipe.branches,
+        branch => branch.name === newVersion.branch);
+      let branch = recipe[branchLocation];
+      branch.mostRecentVersionId = newVersion._id;
+
+      res.status(201).send();
+    }).catch(error => {
       res.status(500).send(error)
     });
   },
