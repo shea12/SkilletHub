69bf01c437d0715e65da86bf3ae82d418b3bd549
austinmr <austin.riedel@gmail.com>
2016-09-29 19:16:30 -0700

    Add EditRecipe route to Index.jsx

--- client/source/Index.jsx
+++ client/source/Index.jsx
@@ -9,14 +9,16 @@ import LandingPage from './components/LandingPage';
 import User from './components/UserPage';
 import RecipeMain from './components/Recipe/RecipeMain'; 
 import CreateRecipeMain from './components/CreateRecipe/CreateRecipeMain'; 
+import EditRecipeMain from './components/EditRecipe/EditRecipeMain'; 
 
 render((
 	<Router history={browserHistory}> 
-	  <Route path="/" component={App} >
+	  <Route path="/" component={App}>
 	  	<IndexRoute component={LandingPage} />
 	  	<Route path="/User" component={User} />
 	  	<Route path="/Recipe" component={RecipeMain} />
 	  	<Route path="/Create" component={CreateRecipeMain} />
+	  	<Route path="/Edit" component={EditRecipeMain} />
 	  </Route>
 	</Router>
 ), document.getElementById('app')); 
