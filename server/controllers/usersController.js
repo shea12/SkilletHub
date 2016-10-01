af24b81702878965722355b1f4cef63580ea974d
Justin Griffin <jbg46209@gmail.com>
2016-09-30 17:16:05 -0700

    Update getProfile to utilize username rather than userId

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -15,15 +15,12 @@ module.exports = {
 
   //Gets all the recipes that belong to that user
   getProfile: (req, res) => {
-    //find user
-    //userId where username: req.params.username
-  //   .then((userId) => {
-  //     //find recipes by user
-  //     return UserRecipes.findOne({userId: userId});
-  //   }).then(result => {
-  //     res.status(200).send(result);
-  //   }).catch(error => {
-  //     res.status(404).send(error);
-  //   });
+    UserRecipes.findOne({
+      username: req.params.username
+    }).then(result => {
+      res.status(200).send(result);
+    }).catch(error => {
+      res.status(404).send(error);
+    });
   }
 };
