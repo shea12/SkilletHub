eb2488be52900d7392748f08a231cb788e8b0209
austinmr <austin.riedel@gmail.com>
2016-10-02 18:31:28 -0700

    Change User page to improve UI and add navigation components allowing to display following list and recipe list

--- client/source/components/User/RecipeListEntry.jsx
+++ client/source/components/User/RecipeListEntry.jsx
@@ -5,18 +5,19 @@ import { Image, Grid, Row, Col, Form, FormGroup, Badge, ProgressBar, FormControl
 const progressBarStyles = ['success', 'warning', 'danger']; 
 
 export default ({recipe, handleUserClick, handleRecipeClick}) => {
+
   return (
         <Row height={50}> 
           <Col xs={6} md={6}> 
-            <h2 id={recipe.name}> {recipe.name || 'Master Recipe'} </h2> 
-            <h4 id={recipe.rootRecipeId} onClick={handleRecipeClick.bind(this)}> {recipe.rootRecipeId} </h4>
+            <h2 id={recipe.rootRecipeId} onClick={handleRecipeClick.bind(this)}> {recipe.name || 'Master Recipe'} </h2> 
+            <h5 id={recipe.rootRecipeId} onClick={handleRecipeClick.bind(this)}> recipe id: {recipe.rootRecipeId} </h5>
             <h6> forked from <Button id={recipe.rootRecipeId} onClick={handleUserClick.bind(this)} disabled>{recipe.sourceID} </Button></h6> 
           </Col> 
           <Col xs={4} md={4} style={{marginTop: 25}}>  
             <ProgressBar bsStyle={progressBarStyles[(Math.floor(Math.random() * 4))]} now={(Math.floor(Math.random() * 100))} />
           </Col>
           <Col xs={2} md={2} style={{marginTop: 20}}> 
-            <h4> forks <Badge>{17}</Badge></h4> 
+            <h4> forks <Badge>{Math.floor(Math.random() * 20)}</Badge></h4> 
           </Col>
         </Row>
     )
