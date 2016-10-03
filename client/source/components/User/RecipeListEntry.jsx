0a4af5e2002d8bbf15e2e1392c7925b80c334d74
austinmr <austin.riedel@gmail.com>
2016-10-02 19:42:53 -0700

    Add additional data to recipe elements within User recipe list components to allow navigation to a specific user's recipes

--- client/source/components/User/RecipeListEntry.jsx
+++ client/source/components/User/RecipeListEntry.jsx
@@ -4,13 +4,13 @@ import { Image, Grid, Row, Col, Form, FormGroup, Badge, ProgressBar, FormControl
 
 const progressBarStyles = ['success', 'warning', 'danger']; 
 
-export default ({recipe, handleUserClick, handleRecipeClick}) => {
+export default ({recipe, username, handleUserClick, handleRecipeClick}) => {
 
   return (
         <Row height={50}> 
           <Col xs={6} md={6}> 
-            <h2 id={recipe.rootRecipeId} onClick={handleRecipeClick.bind(this)}> {recipe.name || 'Master Recipe'} </h2> 
-            <h5 id={recipe.rootRecipeId} onClick={handleRecipeClick.bind(this)}> recipe id: {recipe.rootRecipeId} </h5>
+            <h2 id={recipe.rootRecipeId} data-username={username} onClick={handleRecipeClick.bind(this)}> {recipe.name || 'Master Recipe'} </h2> 
+            <h5 id={recipe.rootRecipeId} data-username={username} onClick={handleRecipeClick.bind(this)}> recipe id: {recipe.rootRecipeId} </h5>
             <h6> forked from <Button id={recipe.rootRecipeId} onClick={handleUserClick.bind(this)} disabled>{recipe.sourceID} </Button></h6> 
           </Col> 
           <Col xs={4} md={4} style={{marginTop: 25}}>  
