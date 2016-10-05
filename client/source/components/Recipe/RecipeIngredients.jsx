7d2dc65cba993e4de80476b5868a8c2990945b94
austinmr <austin.riedel@gmail.com>
2016-10-05 12:50:08 -0700

    Implement Version Control functionality on Recipe component.

--- client/source/components/Recipe/RecipeIngredients.jsx
+++ client/source/components/Recipe/RecipeIngredients.jsx
@@ -25,30 +25,26 @@ class RecipeIngredients extends React.Component {
 
 	render() {
 	  return (
-	  		<Grid>
-	  		<Col xs={12} md={12}>
-			<Table bordered> 
-			  <thead> 
-			    <tr>
-			    	<th data-field="ingredient">Ingredient</th>
-			    	<th data-field="quantity">Quantity</th>
-			    	<th data-field="unit">Unit</th>
-			    </tr>
-			  </thead>
-			  <tbody> 
-			    {this.props.ingredientList.map((ingredient) => {
-			      return (
-				  	<tr key={ingredient.position}>
-				  		<td> {this._renderIngredientName(ingredient)} </td>
-				  		<td> {ingredient.amount} </td>
-				  		<td> {ingredient.unit} </td>
-				  	</tr>
-			      	)
-			    })}
-			  </tbody>
-			</Table>
-			</Col>
-			</Grid>
+					<Table bordered width={'100%'}> 
+					  <thead> 
+					    <tr>
+					    	<th data-field="ingredient">Ingredient</th>
+					    	<th data-field="quantity">Quantity</th>
+					    	<th data-field="unit">Unit</th>
+					    </tr>
+					  </thead>
+					  <tbody> 
+					    {this.props.ingredientList.map((ingredient) => {
+					      return (
+						  	<tr key={ingredient.position}>
+						  		<td> {this._renderIngredientName(ingredient)} </td>
+						  		<td> {ingredient.amount} </td>
+						  		<td> {ingredient.unit} </td>
+						  	</tr>
+					      	)
+					    })}
+					  </tbody>
+					</Table>
 	  ); 
 	}
 
