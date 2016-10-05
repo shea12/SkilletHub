3aed060692864fcf8e7c46832e6b563c60635045
shea12 <cshearun12@gmail.com>
2016-10-05 11:20:47 -0700

    rendering recipe name in cookme, pause on auth middleware

--- server/middleware.js
+++ server/middleware.js
@@ -1,14 +1,23 @@
 var bodyParser = require('body-parser');
 var path = require('path'); 
+let db = require(`${__dirname}/schemas.js`);
+let User = db.User;
 
 module.exports = function(app, express) {
   console.log('getting request!'); 
   app.use(express.static(path.join(__dirname, '../client/deploy')));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+
+  // On every request, we verify that the user is logged in
+  //  by checking their token
+  var verifyUserToken = function(req, res, next) {
+    // req.body needs to include username
+
+
+  };
+
+
+  // app.use(verifyUserToken);
 };
 
-// module.exports = function(app, express) {
-//   console.log('authenticating user session');
-  
-// }
