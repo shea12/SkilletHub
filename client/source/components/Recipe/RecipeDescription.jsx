b905932c390edca119dfa7a0f8404b2090f237a6
austinmr <austin.riedel@gmail.com>
2016-10-07 12:46:51 -0700

    Modify RecipeMain to make use of new version and branch control functionality Refactor VersionControl component Implement Recipe Stats component

--- client/source/components/Recipe/RecipeDescription.jsx
+++ client/source/components/Recipe/RecipeDescription.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Table, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Table, Button, Well } from 'react-bootstrap';
 
 export default ({recipeDescription, handleClick}) => {
   return (
-      <div> 
+        <div>
         <Row style={{borderBottom: "4px solid rgba(128,128,128, 0.2)", height: '80%', textAlign: "center", marginBottom: 10}}> 
           <Col xs={3} md={3}> 
             <h4> serves </h4> 
@@ -27,17 +27,6 @@ export default ({recipeDescription, handleClick}) => {
         <Row> 
           <p> {recipeDescription.description.value || `A basic ${recipeDescription.name} recipe`} </p> 
         </Row>
-        <Row style={{marginTop: 15}}> 
-          <Col xs={3} md={3}> 
-            <Button style={{background: 'rgba(112, 189, 71, 0.8)', width: '100%', height: '50%', color: 'white', textShadow: 'none'}} bsSize='large' onClick={handleClick.bind(this)} id="fork" type="submit">Fork</Button>
-          </Col>
-          <Col xs={3} md={3} mdOffset={1} xsOffset={1}> 
-            <Button style={{background: 'rgba(112, 189, 71, 0.8)', width: '100%', height: '50%', color: 'white', textShadow: 'none'}} bsSize='large' onClick={handleClick.bind(this)} id="cook" type="submit">Cook</Button>
-          </Col>
-          <Col xs={3} md={3} mdOffset={1} xsOffset={1}> 
-            <Button style={{background: 'rgba(112, 189, 71, 0.8)', width: '100%', height: '50%', color: 'white', textShadow: 'none'}} bsSize='large'onClick={handleClick.bind(this)} id="edit" type="submit"> Edit </Button>
-          </Col>
-        </Row>
-      </div> 
+        </div>
   ); 
 }
