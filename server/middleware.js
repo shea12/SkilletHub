f7e15fa1473ec366fc4c226c79f733d5cc48daa0
shea12 <cshearun12@gmail.com>
2016-10-07 15:29:54 -0700

    comment out authMiddleware fn, req auth handled in App.jsx

--- server/middleware.js
+++ server/middleware.js
@@ -9,35 +9,42 @@ module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
-  // On every request, we verify that the user is logged in
-  //  by checking their token
-  var authenticateUser = function(req, res, next) {
-    console.log('authenticateUser middleware running');
-    if (req.params.username) {
-      //search db Users db for username, retrieve token, if token is not null
-      // then they are currently authenticated and logged in
-      User.find({username: req.body.username}, 'token', function(error, response) {
-        if (error) {
-          console.log('Could not find username in db: ', error);
-          res.status(400).send(error);
-        }
-        if (!response.token) {
-          console.log('Found user, token inactive, user not authenticated: ', response);
-          res.status(400).send(response);
-        } else {
-          console.log('Found user, token active, user authenticated: ', response);
-          res.status(200).send(response);
-        }
-      });
-    } else {
-      console.log('req.params.username is false: ', req.params);
-      return res.status(500).send();
-    }
-  };
 
 
-  app.use('/:username/*', authenticateUser, function(req, res) {
-    res.status(200).send();
-  });
+    /*
+    Friday at 3:11pm, not currently using this middleware for authentication on each request.
+    Checking that users are logged in is being handled by the 'handleNavigation' function in App.jsx.
+    
+
+    var authenticateUser = function(req, res, next) {
+      // On every request, we verify that the user is logged in
+      //  by checking their token
+      console.log('authenticateUser middleware running');
+      if (req.params.username) {
+        //search db Users db for username, retrieve token, if token is not null
+        // then they are currently authenticated and logged in
+        User.find({username: req.body.username}, 'token', function(error, response) {
+          if (error) {
+            console.log('Could not find username in db: ', error);
+            res.status(400).send(error);
+          }
+          if (!response.token) {
+            console.log('Found user, token inactive, user not authenticated: ', response);
+            res.status(400).send(response);
+          } else {
+            console.log('Found user, token active, user authenticated: ', response);
+            res.status(200).send(response);
+          }
+        });
+      } else {
+        console.log('req.params.username is false: ', req.params);
+        return res.status(500).send();
+      }
+    };
+
+    app.use('/', authenticateUser, function(req, res) {
+      res.status(200).send();
+    });
+  */
 };
 
