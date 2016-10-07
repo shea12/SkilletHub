08a8e366522ec59027a3e5d1dbb9a50364cde653
shea12 <cshearun12@gmail.com>
2016-10-07 14:33:55 -0700

    working on auth middleware

--- server/middleware.js
+++ server/middleware.js
@@ -13,30 +13,30 @@ module.exports = function(app, express) {
   //  by checking their token
   var authenticateUser = function(req, res, next) {
     console.log('authenticateUser middleware running');
-    if (!req.body.username) {
-      console.log('User could not be authenticated, username not in req.body');
-      return res.status(500).send();
-    } else {
+    if (req.params.username) {
       //search db Users db for username, retrieve token, if token is not null
       // then they are currently authenticated and logged in
-      User.find({username: req.body.username}, token, function(error, response) {
+      User.find({username: req.body.username}, 'token', function(error, response) {
         if (error) {
           console.log('Could not find username in db: ', error);
           res.status(400).send(error);
-        } 
-        if (response.token) {
+        }
+        if (!response.token) {
+          console.log('Found user, token inactive, user not authenticated: ', response);
+          res.status(400).send(response);
+        } else {
           console.log('Found user, token active, user authenticated: ', response);
           res.status(200).send(response);
-        } else {
-          console.log('Found user, token inactive, user not authenticated ', response);
-          res.status(400).send(response);
         }
       });
+    } else {
+      console.log('req.params.username is false: ', req.params);
+      return res.status(500).send();
     }
   };
 
 
-  app.use('/:username', authenticateUser, function(req, res) {
+  app.use('/:username/*', authenticateUser, function(req, res) {
     res.status(200).send();
   });
 };
