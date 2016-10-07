4cb1df61c497de99c2ec6f76816765ea50f80bd0
Justin Griffin <jbg46209@gmail.com>
2016-10-07 15:25:05 -0700

    Add search route and searchController

---
+++ server/controllers/searchController.js
@@ -0,0 +1,9 @@
+let db = require(`${__dirname}/../schemas.js`);
+let Recipe = db.Recipe;
+let _ = require('underscore');
+
+module.exports = {
+  search: (req, res) => {
+    
+  }
+};
--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,13 @@ let recipes = require(`${__dirname}/controllers/recipesController.js`);
 let branches = require(`${__dirname}/controllers/branchesController.js`);
 let versions = require(`${__dirname}/controllers/versionsController.js`);
 let pullRequests = require(`${__dirname}/controllers/pullRequestController.js`);
+let search = require(`${__dirname}/controllers/searchController.js`);
 
 module.exports = function(app, express) {
-  // /*** Sign up / Sign in / Logout ***/
+  /*** Search ***/
+  app.get('/search/:string', search.search);
+
+  /*** Sign up / Sign in / Logout ***/
   app.post('/user/signup', users.signup);
   app.post('/user/login', users.login);
   app.post('/user/logout', users.logout);
@@ -15,16 +19,16 @@ module.exports = function(app, express) {
   app.put('/:username/:pullId/update-pull', pullRequests.updatePullRequestStatus);
   app.get('/:username/get-pulls', pullRequests.getAllPullRequests);
 
-  // /*** Users ***/
+  /*** Users ***/
   app.get('/:username/profile', users.getProfile); //gets recipes
-  // app.put('/:username/profile', users.updateProfile);
+  app.put('/:username/profile', users.updateProfile);
 
-  // /*** Recipes ***/
+  /*** Recipes ***/
   app.post('/:username/create-recipe', recipes.createRecipe);
   app.get('/:username/:recipe', recipes.getRecipe); 
   app.delete('/:username/:recipe', recipes.deleteRecipe);
 
-  // /*** Branches ***/
+  /*** Branches ***/
   app.post('/:username/:recipe/:branch', branches.createBranch);
   app.get('/:username/:recipe/:branch', branches.getBranch);
   app.delete('/:username/:recipe/:branch', branches.deleteBranch);
