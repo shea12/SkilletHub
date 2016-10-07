daa1ea9949f6bb1320ad1c3e6e8a43d5fdf54da6
shea12 <cshearun12@gmail.com>
2016-10-07 12:06:54 -0700

    middleware authentication

--- server/middleware.js
+++ server/middleware.js
@@ -11,13 +11,33 @@ module.exports = function(app, express) {
 
   // On every request, we verify that the user is logged in
   //  by checking their token
-  var verifyUserToken = function(req, res, next) {
-    // req.body needs to include username
-
-
+  var authenticateUser = function(req, res, next) {
+    console.log('authenticateUser middleware running');
+    if (!req.body.username) {
+      console.log('User could not be authenticated, username not in req.body');
+      return res.status(500).send();
+    } else {
+      //search db Users db for username, retrieve token, if token is not null
+      // then they are currently authenticated and logged in
+      User.find({username: req.body.username}, token, function(error, response) {
+        if (error) {
+          console.log('Could not find username in db: ', error);
+          res.status(400).send(error);
+        } 
+        if (response.token) {
+          console.log('Found user, token active, user authenticated: ', response);
+          res.status(200).send(response);
+        } else {
+          console.log('Found user, token inactive, user not authenticated ', response);
+          res.status(400).send(response);
+        }
+      });
+    }
   };
 
 
-  // app.use(verifyUserToken);
+  app.use('/:username', authenticateUser, function(req, res) {
+    res.status(200).send();
+  });
 };
 
