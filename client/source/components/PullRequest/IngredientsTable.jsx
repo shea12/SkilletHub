2cb328675eb0131f5f51ab0c2813aa8142c57d73
austinmr <austin.riedel@gmail.com>
2016-10-07 22:53:18 -0700

    Implement Pull Request components for creating and submitting new pull requests from a specified recipe version

--- client/source/components/PullRequest/IngredientsTable.jsx
+++ client/source/components/PullRequest/IngredientsTable.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Grid, Row, Col, Table } from 'react-bootstrap';
+import { Grid, Row, Col, Table, Glyphicon } from 'react-bootstrap';
 
 // TODO: Confirm ingredient object structure, ensure that key refers to the proper value 
 // TODO: Add headers for the 'Ingredient', 'Quantity', 'Unit' and other fields. 
@@ -14,19 +14,20 @@ class IngredientsTable extends React.Component {
     console.log('Firing render function!'); 
     if (ingredient.prep) {
       return (
-        <p> {`${ingredient.name}, ${ingredient.prep}`} </p>
+       <p style={{display: 'inline-block', marginLeft: 5}}>{`${ingredient.name}, ${ingredient.prep}`}</p>
       )
     } else {
       return (
-        <p> {`${ingredient.name}`} </p>
+        <p style={{display: 'inline-block', marginLeft: 5}}>{`${ingredient.name}`}</p>
       ) 
     }
   } 
 
-  _renderIngredientStatus(ingredient) {
+  _renderIngredientColor(ingredient) {
     if (ingredient.edited) {
       // return 'rgba(224, 209, 41, 0.5)'; 
-      return 'rgba(179, 252, 255, 0.7)'; 
+      // return 'rgba(179, 252, 255, 0.7)'; -> blue
+      return 'rgba(255, 240, 179, 0.7)'; 
     } else if (ingredient.added) {
       // return 'rgba(97, 210, 55, 0.5)'; 
       return 'rgba(219, 255, 219, 0.7)'; 
@@ -37,6 +38,27 @@ class IngredientsTable extends React.Component {
     return 'white'; 
   }
 
+  _renderIngredientEffect(ingredient) {
+    if (ingredient.deleted) {
+      return 'line-through'; 
+    }
+  }
+
+  _renderGlyphicon(ingredient) {
+    if (ingredient.edited) {
+      return (
+        <Glyphicon glyph="refresh" bsSize="large" style={{color: 'rgba(255, 221, 87, 1)', display: 'inline-block'}} />
+      )
+    } else if (ingredient.added) {
+      return (
+        <Glyphicon glyph="plus" bsSize="large" style={{color: 'rgba(112, 255, 112, 1)', display: 'inline-block'}} />
+      )
+    } else if (ingredient.deleted) {
+      return (
+        <Glyphicon glyph="remove" bsSize="large" style={{color: 'rgba(255, 73, 66, 1)', display: 'inline-block'}} />
+      )
+    }
+  }
 
   render() {
     return (
@@ -51,8 +73,8 @@ class IngredientsTable extends React.Component {
             <tbody> 
               {this.props.ingredientList.map((ingredient, i) => {
                 return (
-                <tr key={'ingredient' + i} style={{backgroundColor: this._renderIngredientStatus(ingredient)}}>
-                  <td> {this._renderIngredientName(ingredient)} </td>
+                <tr key={'ingredient' + i} style={{backgroundColor: this._renderIngredientColor(ingredient)}}>
+                  <td> {this._renderGlyphicon(ingredient)} {this._renderIngredientName(ingredient)} </td>
                   <td> {ingredient.amount} </td>
                   <td> {ingredient.unit} </td>
                 </tr>
