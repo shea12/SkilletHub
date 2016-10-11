af47bb1293f58bc3f26ba22599c5733a7f71bcf7
Justin Griffin <jbg46209@gmail.com>
2016-10-10 19:20:15 -0700

    Insert tags into db at server start

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ let app = express();
 let mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/testingSkilletHub');
 let port = process.env.PORT || 3000;
+let tags = require(`${__dirname}/tags.js`);
+let Tag = require(`${__dirname}/schemas.js`).Tag;
 
 require('./middleware.js')(app, express);
 require('./routes.js')(app, express);
 
 app.listen(port, function() {
   console.log('Now running on port: ', port);
+
+  Tag.find().then(results => {
+    return results.length < 1 ? Tag.create(tags.tags) : 'existing';
+  }).then(result => {
+    if (result !== 'existing') {
+      console.log('Tags inserted');
+    }
+  }).catch(error => {
+    console.log(error);
+  });
 });
