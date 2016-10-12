fe792373a871c5a371d3fc97ccb3ecfda5e0ad25
austinmr <austin.riedel@gmail.com>
2016-10-12 15:01:59 -0700

    Modify styling and layout for User components

--- client/source/components/User/PullRequestEntry.jsx
+++ client/source/components/User/PullRequestEntry.jsx
@@ -1,31 +1,65 @@
 import React, {Component} from 'react';
 import { Image, Grid, Row, Col, Form, FormGroup, Badge, ProgressBar, FormControl, Button, Container, ControlLabel, DropdownButton, MenuItem, Nav, NavItem } from 'react-bootstrap';
 
-export default ({pullRequest, username, handleUserClick, handlePullRequestClick}) => {
+class PullRequestEntry extends React.Component {
 
-  return (
-        <Row height={50}> 
-          <Col xs={6} md={6}> 
-            <h2 id={pullRequest.sendingUser} onClick={handleUserClick.bind(this)}> sent from {pullRequest.sendingUser} </h2> 
-            <h4 id={pullRequest.sendingUser}> pull request submitted at {pullRequest.createdAt} </h4>
-          </Col> 
-          <Col xs={4} md={4} style={{marginTop: 20}}> 
-            <Button 
-              data-username={username}
-              data-recipe={pullRequest.targetVersion}
-              data-branch={'master'}
-              data-version={pullRequest.targetVersion}
-              data-pulluser={pullRequest.sendingUser}
-              data-pullrecipe={pullRequest.targetVersion}
-              data-pullrequest={JSON.stringify(pullRequest)}
-              onClick={handlePullRequestClick.bind(this)}> 
-                Manage Pull Request 
-            </Button> 
-          </Col>
-          <Col xs={2} md={2} style={{marginTop: 20}}> 
-            <h4> status </h4> 
-            <Badge>{pullRequest.status}</Badge>
-          </Col>
-        </Row>
+  constructor(props) {
+    super(props);
+  }
+
+  _renderDate(createdAt){
+    if (createdAt) {
+      var parseDate = createdAt.split('T')[0].split('-'); 
+      return (
+        <h6 style={{textAlign: 'center'}}> {`${this.props.month[parseDate[1] - 1]} ${parseDate[2]}, ${parseDate[0]}`}</h6> 
+      )
+    }
+  }
+
+  _renderStatus(status) {
+    if (status === 'open') {
+      return (
+       <p style={{textAlign: 'center'}}> <Badge style={{backgroundColor: 'green', color: 'white'}}>{this.props.pullRequest.status}</Badge></p>
+      )
+    } else {
+      return (
+        <p style={{textAlign: 'center'}}><Badge>{this.props.pullRequest.status}</Badge></p>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <Row height={50} style={{borderBottom: "1px solid rgba(128,128,128, 0.2)"}}> 
+        <Col xs={6} md={6}> 
+          <h2 id={this.props.pullRequest.originalName}> {this.props.pullRequest.originalName} </h2> 
+          <h4 id={this.props.pullRequest.sendingUser} onClick={this.props.handleUserClick.bind(this)}>> sent by <span style={{color: 'blue'}}>{this.props.pullRequest.sendingUser} </span></h4>
+        </Col> 
+        <Col xs={2} md={2} style={{marginTop: 20}}> 
+          <h4 style={{textAlign: 'center'}}> created </h4> 
+          {this._renderDate(this.props.pullRequest.createdAt)}
+        </Col>
+        <Col xs={2} md={2} style={{marginTop: 20}}> 
+          <h4 style={{textAlign: 'center'}}> status </h4> 
+          {this._renderStatus(this.props.pullRequest.status)}
+        </Col>
+        <Col xs={2} md={2} style={{marginTop: 30}}> 
+          <Button 
+            bsStyle="success"
+            data-username={this.props.username}
+            data-recipe={this.props.pullRequest.targetVersion}
+            data-branch={'master'}
+            data-version={this.props.pullRequest.targetVersion}
+            data-pulluser={this.props.pullRequest.sendingUser}
+            data-pullrecipe={this.props.pullRequest.targetVersion}
+            data-pullrequest={JSON.stringify(this.props.pullRequest)}
+            onClick={this.props.handlePullRequestClick.bind(this)}> 
+              Manage  
+          </Button> 
+        </Col>
+      </Row>
     )
+  }
 }
+
+export default PullRequestEntry;
