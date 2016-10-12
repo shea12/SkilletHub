fb3212dbe3b948c7daec7ff3dbf0dba2c75f833d
austinmr <austin.riedel@gmail.com>
2016-10-11 21:08:08 -0700

    Fix bug preventing users from accessing root recipes shown in the Explore component Fixed bug involving the cook button

--- client/source/components/SearchExplore/searchResultsList.jsx
+++ client/source/components/SearchExplore/searchResultsList.jsx
@@ -13,8 +13,8 @@ export default ({recipeList, name, handleRecipeForkClick, handleRecipeCookClick}
             <h4>cooked by // {recipe.username}</h4>
             <p>{recipe.description.value.split('.').slice(0,2).join('.')}...Click to read more</p>
             <p>
-              <Button bsStyle="success" onClick={handleRecipeCookClick.bind(this)} data-username={recipe.username} data-recipe={recipe.rootVersion} data-branch={recipe.branch} data-version={recipe._id}>Cook</Button>&nbsp;
-              <Button bsStyle="default" onClick={handleRecipeForkClick.bind(this)} data-username={recipe.username} data-recipe={recipe.rootVersion} data-branch={recipe.branch} data-version={recipe._id}>Fork</Button>
+              <Button bsStyle="success" onClick={handleRecipeCookClick.bind(this)} data-username={recipe.username} data-recipe={recipe.rootVersion || recipe._id} data-branch={recipe.branch} data-version={recipe._id}>Cook</Button>&nbsp;
+              <Button bsStyle="default" onClick={handleRecipeForkClick.bind(this)} data-username={recipe.username} data-recipe={recipe.rootVersion || recipe._id} data-branch={recipe.branch} data-version={recipe._id}>Fork</Button>
             </p>
           </Thumbnail>
         </Col>
