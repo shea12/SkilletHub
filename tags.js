d36fea0afe78ac573eb31ba6f17ea462050f34f1
shea12 <cshearun12@gmail.com>
2016-10-11 17:41:06 -0700

    add tags options to create recipe

---
+++ tags.js
@@ -0,0 +1,323 @@
+var tags = [
+  {
+    "_id": "none",
+    "name": "Select Tag",
+    "type": "none",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fac",
+    "name": "american",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fad",
+    "name": "british",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fae",
+    "name": "irish",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8faf",
+    "name": "scottish",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb0",
+    "name": "welsh",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb1",
+    "name": "spanish",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb2",
+    "name": "portugese",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb3",
+    "name": "french",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb4",
+    "name": "german",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb5",
+    "name": "italian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb6",
+    "name": "dutch",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb7",
+    "name": "swedish",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb8",
+    "name": "russian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fb9",
+    "name": "chinese",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fba",
+    "name": "japanese",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fbb",
+    "name": "korean",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fbc",
+    "name": "thai",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fbd",
+    "name": "vietnamese",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fbe",
+    "name": "filipino",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fbf",
+    "name": "indonodesian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc0",
+    "name": "hawaiian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc1",
+    "name": "mexican",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc2",
+    "name": "brazilian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc3",
+    "name": "argentinian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc4",
+    "name": "peruvian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc5",
+    "name": "moroccan",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc6",
+    "name": "ethiopian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc7",
+    "name": "turkish",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc8",
+    "name": "syrian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fc9",
+    "name": "greek",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fca",
+    "name": "hungarian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fcb",
+    "name": "iranian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fcc",
+    "name": "indian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fcd",
+    "name": "pakistani",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fce",
+    "name": "puerto rican",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fcf",
+    "name": "afghani",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd0",
+    "name": "persian",
+    "type": "cuisine",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd1",
+    "name": "vegetarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd2",
+    "name": "ovo vegetarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd3",
+    "name": "lacto vegetarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd4",
+    "name": "ovo-lacto vegetarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd5",
+    "name": "fruitarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd6",
+    "name": "vegan",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd7",
+    "name": "pescetarian",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd8",
+    "name": "gluten-free",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fd9",
+    "name": "raw",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fda",
+    "name": "paleo",
+    "type": "diet",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fdb",
+    "name": "kosher",
+    "type": "religious",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fdc",
+    "name": "halal",
+    "type": "religious",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fdd",
+    "name": "low carb",
+    "type": "nutrition",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fde",
+    "name": "low fat",
+    "type": "nutrition",
+    "__v": 0
+  },
+  {
+    "_id": "57fd1ff9dc910f1bf56a8fdf",
+    "name": "low calorie",
+    "type": "nutrition",
+    "__v": 0
+  }
+];
+
+
+module.exports = tags;
